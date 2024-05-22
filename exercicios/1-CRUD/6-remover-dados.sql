115fc67cffbb45a6e44585ab32dceecdd3d7c656
Sarah Santana <72801105+Sarah781@users.noreply.github.com>
2024-05-22 13:57:34 +0000

    exercicio 6

--- exercicios/1-CRUD/6-remover-dados.sql
+++ exercicios/1-CRUD/6-remover-dados.sql
@@ -1,4 +1,7 @@
 -- Da tabela invoice, remova o invoice cujo o InvoiceID corresponda ao valor 33
-
+DELETE from invoices
+WHERE InvoiceID = 33;
 
 -- Da tabela invoice, remova todos os invoices cujo o Total seja menor do que 1
+DELETE from invoices
+WHERE Total < 1;
